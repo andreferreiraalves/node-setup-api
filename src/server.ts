ca6a97e5a30a4aa1df0048ad1fe7c2074d0cdb07
André Ferreira <andre.ferreira.alves@gmail.com>
2021-03-09 14:05:55 -0300

    possível configuração de rotas

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import routes from './routers';
 
 const app = express();
 
+app.use(express.json());
+app.use(routes);
+
 app.get('/', (request, response) => {
-    return response.json({ message: 'Olá mundo' });
+    return response.json('Server running !');
 });
 
 app.listen(3000);
